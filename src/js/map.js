629e4938399830617a8fa44984045908402515da
Ian King <ian.antking@gmail.com>
2019-04-23 20:46:55 +0100

    made map browser friendly

--- src/js/map.js
+++ src/js/map.js
@@ -1,40 +1,45 @@
-const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-const MAP_SQRT = 7;
-class Map {
-  constructor() {
-    this.xAxis = LETTERS;
-    this.yAxis = MAP_SQRT;
-    this.explored = [];
-  }
-
-  _detectDuplicate(coordinate) {
-    return this.explored.includes(coordinate);
-  }
-
-  _randomCoord() {
-    const letter = this.xAxis[Math.floor(Math.random() * this.xAxis.length)];
-    const number = Math.floor(Math.random() * this.yAxis);
-    const coordinate = `${letter}${number}`;
-    return coordinate;
-  }
-
-  _generateCoordinate() {
-    let coordinate;
-    let duplicate = true;
-    while (duplicate) {
-      coordinate = this._randomCoord();
-      duplicate = this._detectDuplicate(coordinate);
+(function exportMap() {
+  const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  const MAP_SQRT = 7;
+  class Map {
+    constructor() {
+      this.xAxis = LETTERS;
+      this.yAxis = MAP_SQRT;
+      this.explored = [];
     }
-    return coordinate;
-  }
 
-  explore(coordinate = this._generateCoordinate()) {
-    if (this._detectDuplicate(coordinate.toLocaleLowerCase())) {
-      throw new Error('Coordinates have already been explored!');
-    } else {
-      this.explored.push(coordinate.toLowerCase());
+    _detectDuplicate(coordinate) {
+      return this.explored.includes(coordinate);
+    }
+
+    _randomCoord() {
+      const letter = this.xAxis[Math.floor(Math.random() * this.xAxis.length)];
+      const number = Math.floor(Math.random() * this.yAxis);
+      const coordinate = `${letter}${number}`;
+      return coordinate;
+    }
+
+    _generateCoordinate() {
+      let coordinate;
+      let duplicate = true;
+      while (duplicate) {
+        coordinate = this._randomCoord();
+        duplicate = this._detectDuplicate(coordinate);
+      }
+      return coordinate;
+    }
+
+    explore(coordinate = this._generateCoordinate()) {
+      if (this._detectDuplicate(coordinate.toLocaleLowerCase())) {
+        throw new Error('Coordinates have already been explored!');
+      } else {
+        this.explored.push(coordinate.toLowerCase());
+      }
     }
   }
-}
-
-module.exports = Map;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Map;
+  } else {
+    window.Map = Map;
+  }
+})();
